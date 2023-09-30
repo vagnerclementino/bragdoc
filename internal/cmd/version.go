7c42a427e5c4f88deda866c087860f8d115d3d70
Vagner Clementino <vagner.clementino@hotmart.com>
2023-09-30 16:07:43 -0300

    feat(#2): add version command implementation

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -1,27 +1,43 @@
 /*
 Copyright © 2023 Vagner Clementino vagner.clemetino@gmail.com
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
 
+var (
+	Version = "unknown"
+	Build   = "unknown"
+)
+
+const defaultWidthTable = 8
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: `ℹ️  This command is used to display the current version of the application`,
+	Long:  `When executed, it will provide you with information about the version number of the application you're running. 🚀`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		writer := new(tabwriter.Writer)
+
+		writer.Init(os.Stdout, defaultWidthTable, defaultWidthTable, 0, '\t', 0)
+
+		defer func(writer *tabwriter.Writer) {
+			err := writer.Flush()
+			if err != nil {
+				return
+			}
+		}(writer)
+
+		fmt.Fprintln(writer, "Version:\t", Version)
+		fmt.Fprintln(writer, "Build:\t", Build)
+
 	},
 }
 
